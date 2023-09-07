40a822d1110419182cd57800142b515530319941
tydonnelly11 <tydonnelly11@gmail.com>
2023-09-07 11:38:38 -0500

    fix

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
-import { createApp } from 'vue'
+import { createApp } from "https://unpkg.com/vue@3/dist/vue.global.js"
 import './style.css'
 import App from './App.vue'
+
 
 createApp(App).mount('#app')
